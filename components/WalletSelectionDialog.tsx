0ad32a0d9831e7e0949b5ce827daa11e05262191
carollpq <107604758+carollpq@users.noreply.github.com>
2025-08-07 10:30:34 +0800

    Improve WalletSelectionDialog UI

--- components/WalletSelectionDialog.tsx
+++ components/WalletSelectionDialog.tsx
@@ -1,41 +1,43 @@
-import { Button, Dialog, Stack } from "@mui/material";
-import { openWalletConnectModal } from "@/services/wallets/walletconnect/walletConnectClient";
+import {Button, Dialog, Stack} from "@mui/material";
+import {openWalletConnectModal} from "@/services/wallets/walletconnect/walletConnectClient";
 import MetamaskLogo from "/public/metamask-logo.svg";
 import Image from "next/image";
 
 
 interface WalletSelectionDialogProps {
-  open: boolean;
-  setOpen: (value: boolean) => void;
-  onClose: (value: string) => void;
+    open: boolean;
+    setOpen: (value: boolean) => void;
+    onClose: (value: string) => void;
 }
 
 export const WalletSelectionDialog = (props: WalletSelectionDialogProps) => {
-  const { onClose, open, setOpen } = props;
+    const {onClose, open, setOpen} = props;
 
-  return (
-    <Dialog onClose={onClose} open={open}>
-      <Stack p={2} gap={1}>
-        <Button
-          variant="contained"
-          onClick={() => {
-            openWalletConnectModal()
-            setOpen(false);
-          }}
-        >
-          <Image
-            src="/walletconnect-logo.svg"
-            alt='walletconnect logo'
-            className='walletLogoImage'
-            width={20}
-            height={20}
-            style={{
-              marginLeft: '-6px'
-            }}
-          />
-          WalletConnect
-        </Button>
-      </Stack>
-    </Dialog>
-  );
+    return (
+        <Dialog onClose={onClose} open={open}>
+            <Stack p={3} gap={2}>
+                <h2 className="text-xl font-bold text-gray-900">Select Which Wallet to Use</h2>
+                <Button
+                    variant="contained"
+                    onClick={() => {
+                        openWalletConnectModal()
+                        setOpen(false);
+                    }}
+                    className="flex items-center justify-center gap-2"
+                >
+                    <Image
+                        src="/walletconnect-logo.svg"
+                        alt='walletconnect logo'
+                        className='walletLogoImage'
+                        width={20}
+                        height={20}
+                        style={{
+                            marginLeft: '-6px'
+                        }}
+                    />
+                    <span>WalletConnect</span>
+                </Button>
+            </Stack>
+        </Dialog>
+    );
 }
